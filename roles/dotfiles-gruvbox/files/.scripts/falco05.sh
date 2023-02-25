f30919f7c46f1b54b53725fce9e89e25817509a6
retiarus <retiarus@gmail.com>
2023-02-25 20:18:55 -0300

    update script to start synergy

--- roles/dotfiles-gruvbox/files/.scripts/falco05.sh
+++ roles/dotfiles-gruvbox/files/.scripts/falco05.sh
@@ -1,5 +1,6 @@
 #!/bin/sh 
-ssh peregrinus@falco05 'tmux new-session -s video -d; tmux split-window -h -t video:0; tmux send-keys -t video:0 "cd ~" Enter; tmux send-keys -t video:0 "xinit" Enter'
+ssh peregrinus@falco05 'tmux new-session -s video -d; tmux split-window -h -t video:0; tmux send-keys -t video:0.0 "cd ~" Enter; tmux send-keys -t video:0.0 "xinit" Enter; tmux send-keys -t video:0.1 "sleep 10" Enter; tmux send-keys -t video:0.1 "barrierc -f --debug INFO --enable-crypto falco02:24800" Enter'
+
 
 
 
