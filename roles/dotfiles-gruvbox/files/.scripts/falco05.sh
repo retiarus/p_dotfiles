c1eab516f0312bdefedcae432c3039b2f69248e6
retiarus <retiarus@gmail.com>
2023-05-04 09:31:13 -0300

    update barrier start using ssh and tmux

--- roles/dotfiles-gruvbox/files/.scripts/falco05.sh
+++ roles/dotfiles-gruvbox/files/.scripts/falco05.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
-ssh peregrinus@falco05 'tmux new-session -s video -d; tmux split-window -h -t video:0; tmux send-keys -t video:0.0 "cd ~" Enter; tmux send-keys -t video:0.0 "xinit" Enter; tmux send-keys -t video:0.1 "sleep 10" Enter; tmux send-keys -t video:0.1 "[[ `pgrep -x barrierc` == "" ]] && barrierc -f --debug INFO --enable-crypto falco02:24800" Enter'
+ssh peregrinus@falco05 'tmux new-session -s video -d; tmux split-window -h -t video:0; tmux send-keys -t video:0.0 "cd ~" Enter; tmux send-keys -t video:0.0 "xinit" Enter; tmux send-keys -t video:0.1 "sleep 10" Enter; tmux send-keys -t video:0.1 '"'"'is_running=`pgrep -x barrierc`; [[ $is_running == "" ]] && barrierc -f --debug INFO --enable-crypto falco02:24800'"'"' Enter'
 
 
 
