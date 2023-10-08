cdf2baa8be40624d4faa1d37c3a2c7de28158359
retiarus <retiarus@gmail.com>
2023-10-08 19:13:18 -0300

    add falcolabs.net to hostname

--- roles/dotfiles-gruvbox/files/.scripts/cubietruck.sh
+++ roles/dotfiles-gruvbox/files/.scripts/cubietruck.sh
@@ -1,5 +1,5 @@
 #!/bin/sh 
-ssh peregrinus@cubietruck 'tmux new-session -s video -d; tmux split-window -h -t video:0; tmux send-keys -t video:0.0 "cd ~" Enter; tmux send-keys -t video:0.0 "xinit" Enter; tmux send-keys -t video:0.1 "sleep 10" Enter; tmux send-keys -t video:0.1 '"'"'is_running=`pgrep -x barrierc`; [[ $is_running == "" ]] && barrierc -f --debug INFO --enable-crypto falco02:24800'"'"' Enter'
+ssh peregrinus@cubietruck.falcolabs.net 'tmux new-session -s video -d; tmux split-window -h -t video:0; tmux send-keys -t video:0.0 "cd ~" Enter; tmux send-keys -t video:0.0 "xinit" Enter; tmux send-keys -t video:0.1 "sleep 10" Enter; tmux send-keys -t video:0.1 '"'"'is_running=`pgrep -x barrierc`; [[ $is_running == "" ]] && barrierc -f --debug INFO --enable-crypto falco02.falcolabs.net:24800'"'"' Enter'
 
 
 
