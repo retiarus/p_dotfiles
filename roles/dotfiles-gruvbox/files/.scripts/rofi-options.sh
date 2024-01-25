a120af89f5dbc9ecacbf7532819c9e85795338f1
retiarus <retiarus@gmail.com>
2024-01-25 12:52:50 -0300

    fix bugs in rofi-options.sh

--- roles/dotfiles-gruvbox/files/.scripts/rofi-options.sh
+++ roles/dotfiles-gruvbox/files/.scripts/rofi-options.sh
@@ -2,20 +2,32 @@
 
 host=`hostname`
 
-list_options=("sakura zellij -l ~/.config/zellij/layouts/pmixer-{$host}.kdl" \
-              "mqtt.sh")
+declare -A list_options
+
+list_options=(["mqtt"]="mqtt.sh"\
+              ["zpmixer"]="sakura zellij -l ~/.config/zellij/layouts/${host}.kdl")
 
 options=""
-for item in ${list_options[@]}
+for item in "${!list_options[@]}"
 do
+  if [ -z "$options" ]
+  then
+    options="$item"
+  else
   options="$options|$item"
+  fi  
 done
 
 echo $options
 
+for option in "${!list_options[@]}"; do echo "$option - ${list_options[$option]}"; done
+
 selected_options=$(echo $options | rofi -sep '|' -dmenu -p -multi-select "option")
 
+echo $selected_options
+
 for option in $selected_options
-do 
-  $option
+do
+  echo "${list_options[$option]}"
+  ${list_options[$option]}
 done
