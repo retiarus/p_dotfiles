d01489c09cd327ee8ce4b4f147df101fb73f8a5d
retiarus <retiarus@gmail.com>
2024-01-26 17:28:41 -0300

    fix error in bash test ], and add code to discover hostname

--- roles/dotfiles-gruvbox/files/.scripts/pmixer.sh
+++ roles/dotfiles-gruvbox/files/.scripts/pmixer.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [ -x "$(command -v sakura)"]; then
+host=`hostname`
+
+if [ -x "$(command -v sakura)" ]; then
   sakura -e zellij -l ~/.config/zellij/layouts/${host}.kdl
-elif [ -x "$(command -v wezterm)"]; then
+elif [ -x "$(command -v wezterm)" ]; then
   wezterm start zellij -l ~/.config/zellij/layouts/${host}.kdl
-elif [ -x "$(command -v alacritty)"]; then
+elif [ -x "$(command -v alacritty)" ]; then
   alacritty -e zellij -l ~/.config/zellij/layouts/${host}.kdl
 fi
